24ffd4c1257efc43ecff890d6d513d480b6d5a1f
jed-richards <jed22richards@gmail.com>
2023-12-20 11:52:19 -0600

    added nerd font script

---
+++ scripts/nerd_font.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+# install FiraMono Nerd Font --> others at: https://www.nerdfonts.com/font-downloads
+echo "[-] Download fonts [-]"
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraMono.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraMono.zip
+unzip FiraMono.zip -d ~/.local/share/fonts
+fc-cache -fv
+rm FiraMono.zip
+echo "done!"
