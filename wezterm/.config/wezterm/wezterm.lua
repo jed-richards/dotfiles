c0fb3038d18032f4fe12c8013b20f8e96aa9f858
jed-richards <jed22richards@gmail.com>
2024-12-26 16:35:19 -0600

    feat: the start of wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Macchiato"
+config.font = wezterm.font("FiraMono Nerd Font")
+
+return config
