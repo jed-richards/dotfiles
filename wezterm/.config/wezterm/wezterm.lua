a64c2a9514bfc71eea650c6671fde46afc7d5ce1
jed-richards <jed22richards@gmail.com>
2024-12-30 16:03:22 -0600

    feat: colorscheme, disable tab bar, font size, and window padding

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,20 @@
 local wezterm = require("wezterm")
+local action = wezterm.action
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Macchiato"
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("FiraMono Nerd Font")
+config.font_size = 12
+
+-- Disable tab bar
+config.enable_tab_bar = false
+
+-- Remove padding from window
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 return config
