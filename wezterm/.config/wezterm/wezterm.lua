7cd4c4dcdbee2ca441acd59189c1eab4fdb5fbae
jed-richards <jed22richards@gmail.com>
2024-12-30 16:03:41 -0600

    feat: color picker

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local color_picker = require("commands.color_picker")
 local action = wezterm.action
 local config = wezterm.config_builder()
 
@@ -15,6 +16,33 @@ config.window_padding = {
 	right = 0,
 	top = 0,
 	bottom = 0,
+}
+
+wezterm.on("change-color-next", function(window, pane)
+	local color = color_picker.change_color("next")
+	window:set_config_overrides({
+		color_scheme = color,
+	})
+end)
+
+wezterm.on("change-color-prev", function(window, pane)
+	local color = color_picker.change_color("prev")
+	window:set_config_overrides({
+		color_scheme = color,
+	})
+end)
+
+config.keys = {
+	{
+		key = "RightArrow",
+		mods = "CTRL|SHIFT",
+		action = action.EmitEvent("change-color-next"),
+	},
+	{
+		key = "LeftArrow",
+		mods = "CTRL|SHIFT",
+		action = action.EmitEvent("change-color-prev"),
+	},
 }
 
 return config
