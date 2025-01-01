812d60856265b6ef0c38c765f43177a8a118984b
jed-richards <jed22richards@gmail.com>
2025-01-01 12:14:25 -0600

    fix(scripts): update nerd font script

--- scripts/nerd_font.sh
+++ scripts/nerd_font.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 # install FiraMono Nerd Font --> others at: https://www.nerdfonts.com/font-downloads
 echo "[-] Download fonts [-]"
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraMono.zip"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraMono.zip
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FiraMono.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FiraMono.zip
 unzip FiraMono.zip -d ~/.local/share/fonts
 fc-cache -fv
 rm FiraMono.zip
