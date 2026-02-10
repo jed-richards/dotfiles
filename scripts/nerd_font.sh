5adc2f6f3ddd1045292e9f1fd5db6ed043181948
jed-richards <jrichards@csgactuarial.com>
2026-02-10 08:53:29 -0600

    chore: remove old scripts

--- scripts/nerd_font.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-# install FiraMono Nerd Font --> others at: https://www.nerdfonts.com/font-downloads
-echo "[-] Download fonts [-]"
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FiraMono.zip"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FiraMono.zip
-unzip FiraMono.zip -d ~/.local/share/fonts
-fc-cache -fv
-rm FiraMono.zip
-echo "done!"
